aaff4c3315f40a140f98c704e5b3c0ae1dd84b1e
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-07-12 15:37:47 +0200

    about added

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,11 +1,14 @@
 "use client"
 import styles from './page.module.css'
 import Layout from '@/components/Layout/Layout'
+import Link from 'next/link'
+
 
 export default function Home() {
   return (
     <Layout>
           juvkhjvkhjvkjhvhjkv
+          <Link href='/about'>About</Link>
     </Layout>
   )
 }
